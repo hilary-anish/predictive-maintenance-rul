305bf123a1f65178294faa7a0f984a8cbd027c09
hilary-anish <anishhilary97@gmail.com>
2026-08-14 10:03:06 +0200

    feat: preprocessing with MinMaxScaler

---
+++ src/pdm/data/preprocess.py
@@ -0,0 +1,16 @@
+import joblib
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from pdm.config import FEATURE_SENSORS, PROC
+
+
+def fit_scaler(train: pd.DataFrame) -> MinMaxScaler:
+    scaler = MinMaxScaler().fit(train[FEATURE_SENSORS])
+    joblib.dump(scaler, PROC / "scaler.joblib")
+    return scaler
+
+
+def apply_scaler(df: pd.DataFrame, scaler: MinMaxScaler) -> pd.DataFrame:
+    df = df.copy()
+    df[FEATURE_SENSORS] = scaler.transform(df[FEATURE_SENSORS])
+    return df
